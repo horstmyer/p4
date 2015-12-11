322d9b55fd9ae53961af3b73cab566382be7eabc
Linda Horstmyer <linda@seashelldigital.com>
2015-12-11 10:01:50 -0500

    Working on Navigation

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,17 +16,21 @@
 					@yield('login')
 				</section>
 				<div class="nav">
-        	@if(Auth::check())
-						<p><a href="/"><img src="/images/bonenav.png" width="100" height="40" alt=""/></a></p>
-						<p><a href="/profile"><img src="/images/profile.png" width="100" height="40" alt=""/></a></p>
-						<p><a href="/profile/addpet"><img src="/images/addpet.png" width="100" height="40" alt=""/></a></p>
-						<p><a href="/logout"><img src="/images/logout.png" width="100" height="40" alt=""/></a></p>
-            @else
-						<p><a href="/"><img src="/images/bonenav.png" width="100" height="40" alt=""/></a></p>
-						<p><a href="/register"><img src="/images/register.png" width="100" height="40" alt=""/></a></p>
-            @endif
-
-    </div>
+        				@if(Auth::check() && (sizeof($pets) <= 3))
+								<p><a href="/"><img src="/images/bonenav.png" width="100" height="40" alt=""/></a></p>
+								<p><a href="/profile"><img src="/images/profile.png" width="100" height="40" alt=""/></a></p>
+								<p><a href="/profile/addpet"><img src="/images/addpet.png" width="100" height="40" alt=""/></a></p>
+								<p><a href="/logout"><img src="/images/logout.png" width="100" height="40" alt=""/></a></p>
+								@elseif(Auth::check() && (sizeof($pets) >= 4))
+								<p><a href="/"><img src="/images/bonenav.png" width="100" height="40" alt=""/></a></p>
+								<p><a href="/profile"><img src="/images/profile.png" width="100" height="40" alt=""/></a></p>
+								<p><a href="/logout"><img src="/images/logout.png" width="100" height="40" alt=""/></a></p>
+								@else
+								<p><a href="/"><img src="/images/bonenav.png" width="100" height="40" alt=""/></a></p>
+								<p><a href="/register"><img src="/images/register.png" width="100" height="40" alt=""/></a></p>
+								<p><a href="/logout"><img src="/images/logout.png" width="100" height="40" alt=""/></a></p>
+            		@endif
+    			</div>
   	</div>
     <!-- Begin Paws -->
     <div class="paws">
