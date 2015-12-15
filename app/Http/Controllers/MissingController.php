3918f5bcb418162082540067e494bc1968640dad
Linda Horstmyer <linda@seashelldigital.com>
2015-12-14 22:18:06 -0500

    again

--- app/Http/Controllers/MissingController.php
+++ app/Http/Controllers/MissingController.php
@@ -1,19 +1,33 @@
 <?php
-​
+
 namespace App\Http\Controllers;
-​
+
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use PDF;
-​
+
 class MissingController extends Controller
 {
-​
-    public function flyer()
+
+    public function flyer($pet_id)
     {
-        $html = view('pdfs.missing')->render();
-​
-        return PDF::load($html)->show();
+        // Get this pet
+        $pet  = \App\Pet::find($pet_id);
+
+        // Convert the photo to base64
+        $pet->photo = base64_encode(file_get_contents($pet->photo));
+        $pet->photo = "data:image/jpg;base64,".$pet->photo;
+
+        // Generate the view, passing it `pet` and `user`
+        $html = view('pdfs.missing')
+            ->with('pet',$pet)
+            ->with('user', \Auth::user())
+            ->render();
+
+        //return PDF::load($html)->show();
+        return $html;
     }
+
+
 }
--- resources/views/pdfs/missing.blade.php
+++ resources/views/pdfs/missing.blade.php
@@ -1,2 +1,9 @@
-
-this is a test.
+<h1>MISSING:</h1>
+      <h3>{{ $pet->petName }}</h3>
+      <p><img src="{{ $pet->photo }}"></p>
+      <p>{{ $pet->breed }}</p>
+      <p>Owner:  {{ $user->name }}</p>
+      <p>Address:  {{ $user->address }}</p>
+      <p>Phone:  {{ $user->phone }}</p>
+      <h3>When & Where my pet went missing:</h3>
+      <p class="notebox"></p>
